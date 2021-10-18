c08975fc754c7474a2ca2ec2db073aaa26454305
Flavie BALITRAND <Flavie BALITRAND@192.168.115.104>
2021-10-18 11:16:44 +0200

    Dépot TP1 ex3 en cours

--- TP1_guessMyNumber_BALITRAND/src/tp1_guessmynumber_balitrand/TP1_guessMyNumber_BALITRAND.java
+++ TP1_guessMyNumber_BALITRAND/src/tp1_guessmynumber_balitrand/TP1_guessMyNumber_BALITRAND.java
@@ -21,21 +21,105 @@ public class TP1_guessMyNumber_BALITRAND {
     public static void main(String[] args) {
        
         //test sur 5 nombres aléatoires
+        
+        Random generateurAleat = new Random();
+        Scanner sc = new Scanner(System.in);
+        
+        
+        
        for(int i =1 ; i<=5; i++)
        {
-        Random generateurAleat = new Random();
-        int n = generateurAleat.nextInt(100);
         
-         Scanner sc = new Scanner(System.in);
+        int n = generateurAleat.nextInt(100);
         System.out.println(n);
        
     }
        
      //test comparation entre nbre choisi et nbre aleatoire
      
-     int nbr_al = generateurAleat.nextInt(100);
+    int nbr_al = generateurAleat.nextInt(100);
+    int nbr_joueur =0;  
+    int k=1;
+    int niveau=0;
+    
+    System.out.println("Bonjour, choisissez le niveau: \n 1) Facile \n 2) Moyen \n 3) Difficile \n 4) Cauchemar");
+    niveau=sc.nextInt();
+    
+    
+    
+    System.out.println("Saisissez une valeur entière entre 0 et 100 :");
+    nbr_joueur=sc.nextInt();  
        
-       
-       
+    if (niveau==1){
+        // niveau facile pas de modifications
+        
+        while(nbr_joueur != nbr_al){
+        
+        if (nbr_joueur < nbr_al){
+            
+           System.out.println("Trop petit"); 
+        }
+        
+        else if (nbr_joueur > nbr_al){
+            
+           System.out.println("Trop grand"); 
+        }
+        
+        System.out.println("Réessayez :");
+        nbr_joueur=sc.nextInt();
+        k++;
+        
     }
-}
+    
+    System.out.println("Gagné ! \n" + k + " tentatives");
+    
+    }
+        
+        
+        
+    
+    
+    
+    
+    else if (niveau==2){
+            
+            
+     //niveau moyen limite de 5 tentatives
+     
+     int lim=1;
+    
+    while(nbr_joueur != nbr_al && lim<= 5){
+        
+        if (nbr_joueur < nbr_al){
+            
+           System.out.println("Trop petit"); 
+        }
+        
+        else if (nbr_joueur > nbr_al){
+            
+           System.out.println("Trop grand"); 
+        }
+        
+        lim++;
+        
+      
+        System.out.println("(Plus que " + (5- lim) +" tentatives. \n"+" Réessayez :");
+        nbr_joueur=sc.nextInt();
+        k++;
+        
+    }
+    if (lim==6){
+        System.out.println("Perdu ! La réponse était : " + nbr_al);
+    }
+        
+    System.out.println("Gagné ! /n" + k + " tentatives");
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+
