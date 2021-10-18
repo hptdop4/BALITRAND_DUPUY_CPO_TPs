42896810ffa724fba4233b0a2bbc30f7b0bcd776
Flavie BALITRAND <Flavie BALITRAND@192.168.115.104>
2021-10-18 12:54:27 +0200

    Dépot TP1 ex4 initialisation

---
+++ TP1_stats_BALITRAND/src/tp1_stats_balitrand/TP1_stats_BALITRAND.java
@@ -0,0 +1,22 @@
+/*
+ * Flavie Balitrand
+* TDC
+* TP1
+* 18/10/21
+ */
+package tp1_stats_balitrand;
+
+/**
+ *
+ * @author Flavie BALITRAND
+ */
+public class TP1_stats_BALITRAND {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
