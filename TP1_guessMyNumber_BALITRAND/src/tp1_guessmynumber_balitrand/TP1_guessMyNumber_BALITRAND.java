d8325aef027dd77db8e64add7d691331a5ef55ca
Flavie BALITRAND <Flavie BALITRAND@192.168.115.104>
2021-10-18 12:48:43 +0200

    Dépot TP1 ex3 en cours 2

--- TP1_guessMyNumber_BALITRAND/src/tp1_guessmynumber_balitrand/TP1_guessMyNumber_BALITRAND.java
+++ TP1_guessMyNumber_BALITRAND/src/tp1_guessmynumber_balitrand/TP1_guessMyNumber_BALITRAND.java
@@ -22,7 +22,7 @@ public class TP1_guessMyNumber_BALITRAND {
        
         //test sur 5 nombres aléatoires
         
-        Random generateurAleat = new Random();
+        Random generateurAleat1 = new Random();
         Scanner sc = new Scanner(System.in);
         
         
@@ -30,14 +30,14 @@ public class TP1_guessMyNumber_BALITRAND {
        for(int i =1 ; i<=5; i++)
        {
         
-        int n = generateurAleat.nextInt(100);
+        int n = generateurAleat1.nextInt(100);
         System.out.println(n);
        
     }
        
      //test comparation entre nbre choisi et nbre aleatoire
-     
-    int nbr_al = generateurAleat.nextInt(100);
+     Random generateurAleat= new Random();
+ 
     int nbr_joueur =0;  
     int k=1;
     int niveau=0;
@@ -47,11 +47,14 @@ public class TP1_guessMyNumber_BALITRAND {
     
     
     
-    System.out.println("Saisissez une valeur entière entre 0 et 100 :");
-    nbr_joueur=sc.nextInt();  
        
     if (niveau==1){
         // niveau facile pas de modifications
+        
+        int nbr_al = generateurAleat.nextInt(100);
+             
+        System.out.println("Saisissez une valeur entière entre 0 et 100 :");
+        nbr_joueur=sc.nextInt(); 
         
         while(nbr_joueur != nbr_al){
         
@@ -84,11 +87,49 @@ public class TP1_guessMyNumber_BALITRAND {
     else if (niveau==2){
             
             
-     //niveau moyen limite de 5 tentatives
+     //niveau moyen choix sur 200 nombres
+     
+     int nbr_al = generateurAleat.nextInt(200);
+             
+        System.out.println("Saisissez une valeur entière entre 0 et 200 :");
+        nbr_joueur=sc.nextInt(); 
+        
+        while(nbr_joueur != nbr_al){
+        
+        if (nbr_joueur < nbr_al){
+            
+           System.out.println("Trop petit"); 
+        }
+        
+        else if (nbr_joueur > nbr_al){
+            
+           System.out.println("Trop grand"); 
+        }
+        
+        System.out.println("Réessayez :");
+        nbr_joueur=sc.nextInt();
+        k++;
+        
+    }
+    
+    System.out.println("Gagné ! \n" + k + " tentatives");
+    
+    }
+    
+    
+    
+    else if (niveau==3){
+        
+     //niveau difficile limite de 5 tentatives et choix parmis 200
      
      int lim=1;
+     int nbr_al=0;
+     
+             
+        System.out.println("Saisissez une valeur entière entre 0 et 200 :");
+        nbr_joueur=sc.nextInt();
     
-    while(nbr_joueur != nbr_al && lim<= 5){
+    while(nbr_joueur != nbr_al & lim< 5){
         
         if (nbr_joueur < nbr_al){
             
@@ -103,23 +144,26 @@ public class TP1_guessMyNumber_BALITRAND {
         lim++;
         
       
-        System.out.println("(Plus que " + (5- lim) +" tentatives. \n"+" Réessayez :");
+        System.out.println("(Plus que " + (5- lim) +" tentatives restantes. \n"+" Réessayez :");
         nbr_joueur=sc.nextInt();
         k++;
         
     }
-    if (lim==6){
+    if (lim==5){
         System.out.println("Perdu ! La réponse était : " + nbr_al);
     }
+      else{  
+    System.out.println("Gagné ! \n" + k + " tentatives");
+    }
         
-    System.out.println("Gagné ! /n" + k + " tentatives");
-    
+        
+        
+        
     }
     
     
     
+    }
     
-    
-    
-    
+}
 
