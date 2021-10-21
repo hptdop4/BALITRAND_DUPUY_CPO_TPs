e01122ce4332c71e961df0b13412d215b8107c92
Flavie BALITRAND <Flavie BALITRAND@192.168.115.104>
2021-10-21 15:32:25 +0200

    Dépot TP1 ex4 fin

--- TP1_stats_BALITRAND/src/tp1_stats_balitrand/TP1_stats_BALITRAND.java
+++ TP1_stats_BALITRAND/src/tp1_stats_balitrand/TP1_stats_BALITRAND.java
@@ -6,6 +6,9 @@
  */
 package tp1_stats_balitrand;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author Flavie BALITRAND
@@ -16,7 +19,31 @@ public class TP1_stats_BALITRAND {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+     int [] Tab = new int[6];
+     
+     Random generateurAleat1 = new Random();
+     Scanner sc = new Scanner(System.in);
+     int m=0;
+        
+      
+      System.out.println("Saisissez une valeur entière :");
+        m=sc.nextInt();
+      
+      for (int i=1; i <= m; i++) {
+          //nbr aleatoire tire entre 0 et 5 puis incrementation au tableau
+          int n = generateurAleat1.nextInt(5);
+          Tab[n]++;
+          
+      }
+      
+      for (int k=0; k <= 5; k++) {
+          //calcul du pourcentage engendre
+      double pourcent=((Tab[k]*100)/m);
+      
+      System.out.println("Le choix " + (k+1) +" a été tiré " + (Tab[k]) +" soit "+ pourcent + " % des fois.");
+      }
     }
+    
     
 }
