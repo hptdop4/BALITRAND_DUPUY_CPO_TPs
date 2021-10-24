f3cd514c552baf953e717ae63f685e92e2b4fb54
Flavie BALITRAND <Flavie BALITRAND@192.168.115.104>
2021-10-24 15:31:24 +0200

    Dépot TP2 ex2 debut

---
+++ TP2_convertisseurObjet_BALITRAND/src/tp2_convertisseurobjet_balitrand/Convertisseur.java
@@ -0,0 +1,25 @@
+/*
+ * *Flavie Balitrand
+* TDC
+* TP1
+* 23/10/21
+ */
+package tp2_convertisseurobjet_balitrand;
+
+/**
+ *
+ * @author Flavie BALITRAND
+ */
+public class Convertisseur {
+    
+    
+   
+    
+    public Convertisseur() { 
+ 
+   int nbConversions=0; 
+}
+    
+    
+    
+}
---
+++ TP2_convertisseurObjet_BALITRAND/src/tp2_convertisseurobjet_balitrand/TP2_convertisseurObjet_BALITRAND.java
@@ -0,0 +1,82 @@
+/*
+ * *Flavie Balitrand
+* TDC
+* TP1
+* 23/10/21
+ */
+package tp2_convertisseurobjet_balitrand;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Flavie BALITRAND
+ */
+public class TP2_convertisseurObjet_BALITRAND {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+      double variable=0;  
+      double variable2=0;
+      int choix=0;  
+        
+      Scanner sc = new Scanner(System.in);  
+        
+        
+       Convertisseur premier = new Convertisseur(3); 
+        
+        
+        
+      System.out.println("Bonjour,saisissez une valeur réelle:");
+        variable=sc.nextDouble();   
+        
+    
+        System.out.println("Saisissez la conversion que vous souhaiter effectuer : \n" + "1) De Celcius vers Kelvin \n" + "2) De Kelvin vers Celcius \n" + "3) De Farenheit vers Celcius \n" + "4) De Celcius vers Farenheit \n" + "5) De Kelvin vers Farenheit \n" +"6) De Farenheit vers Kelvin \n");
+        choix=sc.nextInt();
+        
+        if (choix==1){
+        variable2=CelciusVersKelvin(variable);
+        System.out.println(variable+" degres Celcius est egal à "+ variable2+" degres Kelvin." );
+        
+    }
+        else if (choix==2){
+        variable2=KelvinVersCelcius(variable);
+        System.out.println(variable+" degres Kelvin est egal à "+ variable2+" degres Celcius." );
+        
+    }
+       
+        else if (choix==3){
+        variable2=FarenheitVersCelcius(variable);
+        System.out.println(variable+" degres Kelvin est egal à "+ variable2+" degres Celcius." );
+        
+    }
+        
+        else if (choix==4){
+        variable2=CelciusVersFarenheit(variable);
+        System.out.println(variable+" degres Celcius est egal à "+ variable2+" degres Farenheit." );
+        
+    }
+        
+        else if (choix==5){
+        variable2=KelvinVersFarenheit(variable);
+        System.out.println(variable+" degres Kelvin est egal à "+ variable2+" degres Farenheit." );
+        
+    }
+        
+        else if (choix==6){
+        variable2=FarenheitVersKelvin(variable);
+        System.out.println(variable+" degres Farenheit est egal à "+ variable2+" degres Kelvin." );
+        
+    }         
+               
+               
+               
+               
+        
+        
+    }
+    
+}
