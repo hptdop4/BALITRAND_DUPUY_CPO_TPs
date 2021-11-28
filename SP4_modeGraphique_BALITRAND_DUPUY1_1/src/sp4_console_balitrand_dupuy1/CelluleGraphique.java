5500a70c315a54f11d6fa7d51b9c31294638dcb4
Flavie BALITRAND <Flavie BALITRAND@192.168.115.104>
2021-11-28 16:44:52 +0100

    Dépot TP Puissance 4 Graphique

--- SP4_modeGraphique_BALITRAND_DUPUY1_1/src/sp4_console_balitrand_dupuy1/CelluleGraphique.java
+++ SP4_modeGraphique_BALITRAND_DUPUY1_1/src/sp4_console_balitrand_dupuy1/CelluleGraphique.java
@@ -15,6 +15,9 @@ import javax.swing.JButton;
 public class CelluleGraphique extends JButton {
     Cellule celluleAssociee ;
     ImageIcon img_vide = new javax.swing.ImageIcon(getClass ().getResource("/images/celluleVide.png"));  
+    ImageIcon img_trouNoir = new javax.swing.ImageIcon(getClass ().getResource("/images/trouNoir.png"));  
+    ImageIcon img_jetonRouge = new javax.swing.ImageIcon(getClass ().getResource("/images/jetonRouge.png"));  
+    ImageIcon img_jetonJaune = new javax.swing.ImageIcon(getClass ().getResource("/images/jetonJaune.png"));  
     
     public CelluleGraphique (Cellule uneCellule){
     celluleAssociee = uneCellule;
@@ -24,6 +27,27 @@ public class CelluleGraphique extends JButton {
 @Override
 public void paintComponent (Graphics G){
     super.paintComponent(G);
+    
+    if(celluleAssociee.presenceTrouNoir()==true){
+        
+        setIcon (img_trouNoir);
+    }
+    else{
+        
+       String couleur_jeton = celluleAssociee.lireCouleurDuJeton();
+       
+       switch(couleur_jeton){
+          
+           case "Vide" -> setIcon (img_trouNoir);
+           case "Rouge" -> setIcon (img_jetonRouge);
+           case "Jaune" -> setIcon (img_jetonJaune);
+                    
+          
+       }
+             
+        
+    }
+    
     setIcon (img_vide); // on attribue l'image celluleVide.png
 }
 
