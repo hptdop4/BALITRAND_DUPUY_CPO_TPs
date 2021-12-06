ea9d136db008a66a0ae778fb4ccb6e69259fe523
Flavie BALITRAND <Flavie BALITRAND@192.168.115.104>
2021-12-06 10:16:50 +0100

    Initialisation

---
+++ Mini-Projet_BALITRAND_DUPUY/src/mini/projet_balitrand_dupuy/MiniProjet_BALITRAND_DUPUY.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mini.projet_balitrand_dupuy;
+
+/**
+ *
+ * @author Flavie BALITRAND
+ */
+public class MiniProjet_BALITRAND_DUPUY {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
