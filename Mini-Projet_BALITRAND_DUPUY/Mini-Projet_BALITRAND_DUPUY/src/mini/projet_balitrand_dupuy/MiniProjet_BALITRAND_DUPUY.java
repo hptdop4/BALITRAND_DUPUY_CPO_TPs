72d19f4a8f7415bdc721b7535b0b4f21fd648e78
ROXANA <ROXANA@192.168.114.110>
2021-12-29 15:14:59 +0100

    VERIFICATION

---
+++ Mini-Projet_BALITRAND_DUPUY/Mini-Projet_BALITRAND_DUPUY/src/mini/projet_balitrand_dupuy/MiniProjet_BALITRAND_DUPUY.java
@@ -0,0 +1,36 @@
+/*
+ *Mini Projet Lights out
+ * Dupuy et Balitrand
+ */
+package mini.projet_balitrand_dupuy;
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Flavie BALITRAND
+ */
+public class MiniProjet_BALITRAND_DUPUY {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Bienvenue dans l'aventure");
+        System.out.println("Quel est votre nom ?");
+        
+        Scanner sc = new Scanner(System.in);
+        
+        String nom = sc.nextLine();
+        
+        
+        Joueur J1= new Joueur(nom);
+        Partie partieJeu = new Partie(J1);
+        
+        
+        partieJeu.TourPartie();
+    }
+    
+}
